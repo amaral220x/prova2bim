2e393bf93a5d22a9091b91e64fe5db7592dcd43b
amaral220x <biel.ilha2021@gmail.com>
2019-07-13 12:56:14 -0300

    Questão 1 implementada com sucesso

--- q1.php
+++ q1.php
@@ -1,9 +1,26 @@
 <?php
     $ano = $_GET["ano"];
-    $indice = 2016 - $ano;
-    $f = fopen("violencia-domestica-df.csv",r);
-    $d = fgetcsv($f);
-    while($d){
-        echo "$d[0]  $d[indice]"
+    $indice = $ano-2010;
+    $a = 0;
+    if($ano>2016 or $ano<2011){
+        echo "Não temos dados desse ano. Por favor, digite um ano de 2011 a 2016";
+        $a++;
+    }
+    if($a==0){
+        $f = fopen("violencia-domestica-df.csv",r);
+        $d = fgetcsv($f);
+        echo "<table>";
+        echo "<tr><th>$d[0]</th>";
+        echo "<th>Quantidade</th>";
+        echo "</tr>";
+        $d = fgetcsv($f);
+        while($d){
+            echo "<tr>";
+            echo "<th>$d[0]</th>";
+            echo "<th>$d[$indice]</th>";
+            echo "</tr>";
+            $d = fgetcsv($f);
+        }
+        echo "</table>";
     }
 ?>
